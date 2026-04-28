94468824e88570309cd85bb8e60288da4a597987
Dev-WIA <sl.alima26@email.com>
2026-04-28 18:39:22 +0200

    connect backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default {
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "https://eshop1.onrender.com",
         changeOrigin: true,
-      },
-    },
-  },
-})
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  }
+};
